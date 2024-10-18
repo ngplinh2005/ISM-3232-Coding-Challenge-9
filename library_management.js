a62f7b3085718819476b3a38ab3b236f5e49488a
Nguyễn Phương Linh <ngplinh2005@gmail.com>
2024-10-18 00:28:50 -0400

    Create Patron class to borrow and return books

--- library_management.js
+++ library_management.js
@@ -1,6 +1,6 @@
 // Task 1: Create a Book Class
 class Book {
-    constructor(title, author, ISBN, _isAvailable) {
+    constructor(title, author, ISBN) {
         this.title = title
         this.author = author
         this.ISBN = ISBN
@@ -19,7 +19,7 @@ class Book {
 
 // Task 2: Create a Section Class
 class Section {
-    constructor(name, book) {
+    constructor(name) {
         this.name = name
         this.books = []
     }
@@ -34,5 +34,38 @@ class Section {
         this.books.forEach(book => {
             console.log(`${book.getDetails()} - ${book._isAvailable ? "Available" : "Borrowed"}`)
         })
+    }
+}
+
+// Task 3: Create a Patron class
+class Patron {
+    constructor(name) {
+        this.name = name
+        this.borrowedBooks = []
+    }
+    borrowBook(book) {
+        if (book._isAvailable) {
+            this.borrowedBooks.push(book)
+            book._isAvailable = false
+            console.log(`${this.name} borrowed ${book.title}`)
+        }
+        else {
+            console.log(`${book.title} is not available at this time`)
         }
     }
+    returnBook(book) {
+        let found = false
+        for (let i = 0; i < this.borrowedBooks.length; i++) {
+            if (this.borrowedBooks[i] === book) {
+                this.borrowedBooks[i].isAvailable = true
+                console.log(`${this.name} returned "${book.title}"`)
+                found = true
+                this.borrowedBooks = this.borrowedBooks.filter(b => b !== book)
+                break
+            }
+        }
+        if (!found) {
+            console.log(`${this.name} doesn't have "${book.title}" borrowed.`)
+        }
+    }
+}
